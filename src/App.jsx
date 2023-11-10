d7434406dc0031681792342a73b3db866b8ef4b7
Ganesh23Nayak <ganeshrnayak23@gmail.com>
2023-11-10 22:46:09 +0530

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Administrator from './pages/Administrator';
 import Home from './pages/Home';
 import Patient from './pages/Patient';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
+// hello
 function App() {
 	return (
 		<>
